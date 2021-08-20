07521c2e571f3214434c34af4650cbb622efb1da
badboymai2k1 <89232243+badboymai2k1@users.noreply.github.com>
2021-08-20 09:40:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Tâm à!',
+    introTitle: 'Tâm à😘😘!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao tâm cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' https://www.facebook.com/anhthanhnien2k1
+    messLink: 'https://www.facebook.com/anhthanhnien2k1
 }
